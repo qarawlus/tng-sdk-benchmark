dfadef246a30bceebed0fb4c792f62879e7c9fff
harshita <er.harshitapuri@gmail.com>
2019-11-05 00:53:50 +0000

    Edited python script for releasing floating ips

--- release_ips.py
+++ release_ips.py
@@ -1,14 +1,14 @@
-#This script will run in openstack 'stack' user
+#This script can be used from any user
 '''
-Use it directly or through a script as follows: 
+Use this script as follows: 
 cd
-printf "python PATH_TO_RELEASE_IPS_PYTHON_FILE" > release_ips.sh
+printf "source /opt/stack/devstack/accrc/admin/admin\npython PATH_TO_RELEASE_IPS_PYTHON_FILE \n rm floating_ip_list.yaml \n" > release_ips.sh
 chmod +x release_ips.sh
 ./release_ips.sh
 '''
 import os
 import yaml
-os.system("source ~/devstack/accrc/admin/admin")
+#os.system("source /opt/stack/devstack/accrc/admin/admin") #not running from python script
 os.system("openstack floating ip list -f yaml > floating_ip_list.yaml")
 with open("floating_ip_list.yaml") as f:
     yaml_data = yaml.safe_load(f)
