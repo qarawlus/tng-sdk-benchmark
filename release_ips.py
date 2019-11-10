7fd8406b1c1dfd07a3170cda2a39ff7b4b54fff9
Bhuvan Sharma <er.bsharma021@gmail.com>
2019-11-10 22:48:38 +0000

    Working release_ips script

--- release_ips.py
+++ release_ips.py
@@ -4,14 +4,13 @@ Use this script as follows:
 cd
 printf "source /opt/stack/devstack/accrc/admin/admin\npython PATH_TO_RELEASE_IPS_PYTHON_FILE \n rm floating_ip_list.yaml \n" > release_ips.sh
 chmod +x release_ips.sh
-./release_ips.sh
+./release_ips.sh #manual run
 '''
 import os
 import yaml
-#os.system("source /opt/stack/devstack/accrc/admin/admin") #not running from python script
-os.system("openstack floating ip list -f yaml > floating_ip_list.yaml")
+os.system("/usr/local/bin/openstack floating ip list -f yaml > floating_ip_list.yaml")
 with open("floating_ip_list.yaml") as f:
     yaml_data = yaml.safe_load(f)
     for ele in yaml_data:
         if ele.get("Fixed IP Address")==None:
-            os.system("openstack floating ip delete {}".format(ele.get("Floating IP Address")))
+            os.system("/usr/local/bin/openstack floating ip delete {}".format(ele.get("Floating IP Address")))
